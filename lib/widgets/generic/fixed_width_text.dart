3d849a00fd30ba189396976a65594cb32577c3cf
lututui <lututuibr@gmail.com>
2020-08-16 17:34:23 -0300

    Resize FixedWidthText to avoid ellipsis

--- lib/widgets/generic/fixed_width_text.dart
+++ lib/widgets/generic/fixed_width_text.dart
@@ -3,46 +3,60 @@ import 'package:flutter/material.dart';
 /// A text widget that layouts [inlineSpan] with fixed width
 class FixedWidthText extends StatelessWidget {
   /// Creates a new fixed width text widget
-  ///
-  /// [width] should be equal to or larger than the width of the
-  /// unconstrained [inlineSpan]
   FixedWidthText({
     @required InlineSpan inlineSpan,
-    @required num width,
+    @required double width,
     TextDirection textDirection = TextDirection.ltr,
     TextAlign textAlign = TextAlign.center,
     int maxLines = 1,
     Key key,
-  }) : super(key: key) {
+  })  : _maxWidth = width,
+        super(key: key) {
     ArgumentError.checkNotNull(inlineSpan, 'inlineSpan');
-    ArgumentError.checkNotNull(width, 'width');
 
     _textPainter
       ..textDirection = textDirection ?? TextDirection.ltr
       ..textAlign = textAlign ?? TextAlign.center
       ..maxLines = maxLines ?? 1
       ..text = inlineSpan
-      ..layout(minWidth: width.toDouble(), maxWidth: width.toDouble());
+      ..layout(minWidth: width);
   }
 
   final TextPainter _textPainter = TextPainter();
+  final double _maxWidth;
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(painter: _FixedWidthTextPainter(_textPainter));
+    return CustomPaint(
+      painter: _FixedWidthTextPainter(_textPainter, _maxWidth),
+    );
   }
 }
 
 class _FixedWidthTextPainter extends CustomPainter {
-  const _FixedWidthTextPainter(this.painter);
+  const _FixedWidthTextPainter(this.painter, this.maxWidth);
 
   final TextPainter painter;
+  final double maxWidth;
 
   @override
   void paint(Canvas canvas, Size size) {
-    painter.paint(canvas, Offset(painter.width, painter.height) * (-1 / 2));
+    if (maxWidth == 0) return;
+
+    canvas.scale(1 - (painter.width - maxWidth) / maxWidth);
+    painter.paint(
+      canvas,
+      Offset(painter.width, painter.height).scale(-0.5, -0.5),
+    );
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
+  bool shouldRepaint(_FixedWidthTextPainter oldDelegate) {
+    return oldDelegate.painter.text != painter.text ||
+        oldDelegate.painter.textDirection != painter.textDirection ||
+        oldDelegate.painter.textAlign != painter.textAlign ||
+        oldDelegate.painter.maxLines != painter.maxLines ||
+        oldDelegate.maxWidth != maxWidth ||
+        oldDelegate.painter.width != painter.width;
+  }
 }
